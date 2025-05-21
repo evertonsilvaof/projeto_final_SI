4c38ec550db860cc872bb357ca1570f436d9bd9e
EVERTON FERNANDES <evertonsilvaoficiallll@gmail.com>
2025-05-21 20:20:30 -0300

    incial

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-# save this as app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def olá():
-   # return '<h1>Olá, mundo!</h1>'
-   return render_template('index.html')
+@app.route('/')
+def ola():
+    # return '<h1>Olá, Mundo!</h1>'
+    return render_template('index.html')
 
-@app.route("/equipe.html")
-def equipe():
-   # return '<h1>Olá, mundo!</h1>'
-   return render_template('equipe.html')
-
-
+@app.route('/sobre-equipe')
+def sobre_equipe():
+    return render_template('sobre.html')
 
 
 app.run()
